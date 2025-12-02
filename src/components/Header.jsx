d1e61c83b27037f9cd6932af4a187bc183ebb546
안희영 <hyyounii@naver.com>
2025-12-02 12:18:35 +0900

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { memo } from "react";
+
+const Header = memo(({ userid, onLogout, onMyPosts, onAllPosts }) => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="./logo.avif" alt="X Logo" className="logo-img" />
+        <h1 className="logo-name"></h1>
+        {userid && <span className="logo-user">@{userid}</span>}
+      </div>
+      {userid && (
+        <nav className="menu">
+          <button onClick={onAllPosts}>All Posts</button>
+          <button onClick={onMyPosts}>My Posts</button>
+          <button className="menu-item" onClick={onLogout}>
+            Logout
+          </button>
+        </nav>
+      )}
+    </header>
+  );
+});
+
+export default Header;
